aec24e422f824e4c1f568087a990bce1b7987fee
Ssoutnn <voutpn@outlook.com>
2025-11-15 10:36:09 +0000

    cloudfare

---
+++ functions/express/api.js
@@ -0,0 +1,37 @@
+export async function onRequest(context) {
+  const responseMsg = function(msg) {
+    return new Response(JSON.stringify({
+        code: 500,
+        success: false,
+        message: msg
+    }), {
+      headers: { "Content-Type": "application/json" }
+    })
+  }
+  const request = context.request;
+  try {
+    const data = await request.json() || {};
+    const url = 'https://app-go.heytea.com' + data.url;
+    const method = data.method || 'POST';
+    const headers = data.headers || {};
+    const body = data.body || {};
+    let config = {
+      method: method,
+      headers: headers,
+      signal: AbortSignal.timeout(30000)
+    }
+    if(method == 'POST') {
+      config.body = body;
+    }
+    const getResponse = await fetch(url, config);
+    if (!getResponse.ok) {
+      return responseMsg(`请求失败: ${getResponse.status}`);
+    }
+    const getData = await getResponse.json();
+    return new Response(JSON.stringify(getData), {
+      headers: { "Content-Type": "application/json" }
+    });
+  } catch (err) {
+    return responseMsg(`请求出错: ${err.message}`);
+  }
+}
---
+++ functions/express/upload.js
@@ -0,0 +1,51 @@
+export async function onRequest(context) {
+
+    const responseMsg = function (msg) {
+        return new Response(JSON.stringify({
+            code: 500,
+            success: false,
+            message: msg
+        }), {
+            headers: { "Content-Type": "application/json" }
+        })
+    }
+    const request = context.request;
+    try {
+        const formData = await request.formData();
+        const width = formData.get("width");
+        const height = formData.get("height");
+        const sign = formData.get("sign");
+        const t = formData.get("t");
+        const token = formData.get("token");
+
+        const url = `https://app-go.heytea.com/api/service-cps/user/diy?sign=${sign}&t=${t}`;
+        const headers = {
+            'Authorization': token
+        };
+        const file = formData.get("file");
+        if (!file || !sign || !t || !token) {
+            return responseMsg('Missing required parameters');
+        }
+        // 构造 FormData
+        const form = new FormData();
+        form.append('file', file, `${t}.png`);
+        form.append('width', width);
+        form.append('height', height);
+        // 发送请求
+        const getResponse = await fetch(url, {
+            method: "POST",
+            headers: headers,
+            body: form,
+            signal: AbortSignal.timeout(60000)
+        });
+        if (!getResponse.ok) {
+            return responseMsg(`请求失败: ${getResponse.status}`);
+        }
+        const getData = await getResponse.json();
+        return new Response(JSON.stringify(getData), {
+            headers: { "Content-Type": "application/json" }
+        });
+    } catch (err) {
+        return responseMsg(`请求出错: ${err.message}`);
+    }
+}
